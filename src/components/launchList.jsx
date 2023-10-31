505da5bbecafbd3eafae9daebe5964c378f4b0bf
MaksTue <pro100max48@gmail.com>
2023-10-31 23:07:12 +0300

    add launchpads and draw points

--- src/components/launchList.jsx
+++ src/components/launchList.jsx
@@ -1,19 +1,16 @@
 function LaunchList(props) {
-
-    return (
-        <aside className="aside" id="launchesContainer">
-            <h3>Launches</h3>
-            <div id="listContainer">
-
-
-                <ul>
-                    {props.launches.map(launch => {
-                        return <li key={launch.id}>{launch.name}</li>
-                    })}
-                </ul>
-            </div>
-        </aside>
-    )
+  return (
+    <aside className="aside" id="launchesContainer">
+      <h3>Launches</h3>
+      <div id="listContainer">
+        <ul>
+          {props.launches.map((launch) => {
+            return <li key={launch.id}>{launch.name}</li>;
+          })}
+        </ul>
+      </div>
+    </aside>
+  );
 }
 
-export {LaunchList}
+export { LaunchList };
